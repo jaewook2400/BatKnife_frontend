d51e2e70fe09bef947d5ac33cfb07c50a338cf5c
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-06-05 20:49:05 +0900

    update custom_app_bar

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -11,52 +11,65 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   });
 
   @override
-  Size get preferredSize => const Size.fromHeight(48);
+  Size get preferredSize => const Size.fromHeight(58);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SizedBox(
-        height: 48,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            if (showBackButton)
-              Positioned(
-                left: 16,
-                child: IconButton(
-                  icon: const Icon(Icons.arrow_back),
-                  onPressed: () => Navigator.of(context).pop(),
-                  iconSize: 24,
-                  padding: EdgeInsets.zero,
-                  constraints: const BoxConstraints(),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 35),
+        child: SizedBox(
+          height: 48,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              if (showBackButton)
+                Positioned(
+                    left: 0,
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      constraints: BoxConstraints(), // 최소화
+                      icon: SizedBox(
+                        width: 11.46,
+                        height: 20.34,
+                        child: Image.asset(
+                          'assets/icons/back.png', // 경로는 네가 넣은 이미지에 맞춰
+                          fit: BoxFit.contain,
+                          color: Colors.black,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    )
+
+                ),
+              Text(
+                title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Color(0xFF35353F),
+                  fontSize: 16,
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w700,
+                  height: 1.5,
                 ),
               ),
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Color(0xFF35353F),
-                fontSize: 16,
-                fontFamily: 'Pretendard',
-                fontWeight: FontWeight.w700,
-                height: 1.5,
-              ),
-            ),
-            const Positioned(
-              right: 16,
-              child: Opacity(
-                opacity: 0,
-                child: SizedBox(
-                  width: 18,
-                  height: 18,
-                  child: Stack(),
+              const Positioned(
+                right: 16,
+                child: Opacity(
+                  opacity: 0,
+                  child: SizedBox(
+                    width: 18,
+                    height: 18,
+                    child: Stack(),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 }
